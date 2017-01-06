1bb5d0250f547fe26b15d0bcfd2cb7a7973d970c
Robert Young <rwy0717@gmail.com>
2017-01-06 16:49:54 -0500

    Document ab::Process

--- include/ab/process.hpp
+++ include/ab/process.hpp
@@ -5,9 +5,13 @@
 
 namespace ab {
 
+/// Global init and shutdown.
 class Process {
 public:
+  /// Process-wide initialization. Must be the ab first call.
   static Condition init();
+
+  /// Process-wide tear down. Must be the last ab call made.
   static Condition kill();
 
 private:
