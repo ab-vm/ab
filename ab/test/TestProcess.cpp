25db7f8e100e31f72ec473794d4f8ca545bd1f4b
Robert Young <rwy0717@gmail.com>
2017-08-31 12:33:25 -0400

    Use the Ab::Test namespace
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- ab/test/TestProcess.cpp
+++ ab/test/TestProcess.cpp
@@ -1,9 +1,15 @@
 #include "Ab/Process.hpp"
 #include <gtest/gtest.h>
 
+namespace Ab {
+namespace Test {
+
 TEST(TestProcess, startAndStop) {
 	for (int i = 0; i < 3; i++) {
-		EXPECT_EQ(Ab::Process::init(), Ab::Error::SUCCESS);
-		EXPECT_EQ(Ab::Process::kill(), Ab::Error::SUCCESS);
+		EXPECT_EQ(Process::init(), Error::SUCCESS);
+		EXPECT_EQ(Process::kill(), Error::SUCCESS);
 	}
 }
+
+}  // namespace Test
+}  // namespace Ab
