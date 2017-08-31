627ae63c56bd2d4ee12ccd63869e491bdf869af7
Robert Young <rwy0717@gmail.com>
2017-08-31 10:53:24 -0400

    Don't use ninja
    Installing ninja on OSX takes a significant amount of time.
    On a fresh build, ninja is not significantly faster than make anyways.
    
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -2,13 +2,9 @@
 
 set -evx
 
-if "x$TRAVIS_OS_NAME" = "xosx"; then
-	brew install ninja
-fi
-
 git submodule update --init --recursive
 
 mkdir build && cd build
-cmake -G Ninja -C ../ci/travis/cache.cmake ..
+cmake -C ../ci/travis/cache.cmake ..
 cmake --build .
 ctest -V .
