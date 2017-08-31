8978423bbd69087fb97398108bc2e0202034dc31
Robert Young <rwy0717@gmail.com>
2017-08-31 09:52:40 -0400

    Implement a basic string span
    Right now the type only allows a few comparison operations. Eventually,
    I think it makes sense to move this out to external comparison
    operations, to allow for non-const compares.
    
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- pith/include/Pith/StringSpan.hpp
+++ pith/include/Pith/StringSpan.hpp
@@ -1,17 +1,56 @@
 #ifndef PITH_STRINGSPAN_HPP_
 #define PITH_STRINGSPAN_HPP_
 
+#include <Pith/Config.hpp>
 #include <Pith/Span.hpp>
+#include <algorithm>
+#include <cstring>
 
 namespace Pith {
 
-class StringSpan : public Span<char> {
+class StringSpan : public Span<const char> {
 public:
-	constexpr inline auto operator==(const StringSpan&& other) const {
-		PITH_ASSERT_UNREACHABLE_MSG("TODO: Implement here.");
+	using Span<const char>::Span;
+
+	inline StringSpan(const char* s) : Span<const char>(s, std::strlen(s)) {
 	}
 
-	constexpr inline auto compare() const -> int {
+	/// Compare up to n characters in string s.
+	inline auto compare(const char* s, const std::size_t n) const -> int {
+		const std::size_t m = std::min(length(), n);
+		return std::strncmp(value(), s, m);
+	}
+
+	inline auto compare(const char* s) const -> int {
+		return std::strncmp(value(), s, length());
+	}
+
+	inline auto compare(const StringSpan& span) const -> int {
+		return compare(span.value(), span.length());
+	}
+
+	inline auto operator==(const StringSpan& rhs) const -> bool {
+		if (length() != rhs.length()) {
+			return false;
+		}
+		return compare(rhs) == 0;
+	}
+
+	inline auto operator!=(const StringSpan& rhs) const -> bool {
+		return !(*this == rhs);
+	}
+
+	inline auto operator==(const char* rhs) const -> bool {
+		std::size_t n = std::strlen(rhs);
+		if (length() != n) {
+			return false;
+		} else {
+			return compare(rhs, std::strlen(rhs)) == 0;
+		}
+	}
+
+	inline auto operator!=(const char* rhs) const -> bool {
+		return !(*this == rhs);
 	}
 };
 
