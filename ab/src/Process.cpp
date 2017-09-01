ceb8bae76f8e9116b9e8a8d10838e7add2e4c957
Robert Young <rwy0717@gmail.com>
2017-08-31 23:36:07 -0400

    Set the initialized state in Process
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- ab/src/Process.cpp
+++ ab/src/Process.cpp
@@ -10,12 +10,14 @@ bool Process::initialized_ = false;
 // static
 auto Process::init() -> Error {
 	assert(initialized_ == false);
+	initialized_ = true;
 	return Error::SUCCESS;
 }
 
 // static
 auto Process::kill() -> Error {
 	assert(initialized_ == true);
+	initialized_ = false;
 	return Error::SUCCESS;
 }
 
