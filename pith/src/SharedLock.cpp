d60421478ceccb6f0725162f5a29408cbf080ec5
Robert Young <rwy0717@gmail.com>
2017-09-08 11:25:18 -0400

    Add missing SharedLock init/kill methods

--- pith/src/SharedLock.cpp
+++ pith/src/SharedLock.cpp
@@ -1,3 +1,5 @@
+#include <Pith/Config.hpp>
+#include <Pith/Debug.hpp>
 #include <Pith/SharedLock.hpp>
 
 namespace Pith {
@@ -7,5 +9,15 @@ SharedLock::SharedLock() noexcept = default;
 
 // TODO(rwy0717): Implement
 SharedLock::~SharedLock() noexcept = default;
+
+auto SharedLock::init() -> SharedLockError {
+	PITH_TRACE();
+	return SharedLockError::SUCCESS;
+}
+
+auto SharedLock::kill() -> SharedLockError {
+	PITH_TRACE();
+	return SharedLockError::SUCCESS;
+}
 
 }  // namespace Pith
