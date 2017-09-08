e908395e28e322333ad637aed6503f040d164a7e
Robert Young <rwy0717@gmail.com>
2017-09-08 11:55:08 -0400

    Implement StructCell as a NativeCell subclass
    NativeCell is a class of Cells for holding native data. StructCells will
    continue to live as a templated NativeCell intended to hold one native
    struct. NativeCells can be used directly for more complex cases.
    
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- om/include/Om/StructCell.hpp
+++ om/include/Om/StructCell.hpp
@@ -1,45 +1,17 @@
 #ifndef OM_STRUCTCELL_HPP_
 #define OM_STRUCTCELL_HPP_
 
-#include <Om/Cell.hpp>
-#include <Om/VisitPolicy.hpp>
+/// @file
+/// This is a templated type for wrapping c++ types with a NativeCell wrapper.
+
+#include <Om/Config.hpp>
+#include <Om/NativeCell.hpp>
+#include <Pith/Box.hpp>
 
 namespace Om {
 
-/// A wrapper for heap-allocated C/C++ native structs. The wrapper places an object header on the
-/// struct.
-template <typename Type> class StructCell : public Cell {
-public:
-	inline Type* operator->();
-	inline constexpr const Type& operator*() const;
-
-	inline Type& operator*();
-	inline constexpr const Type* operator->() const;
-
-private:
-	ObjectHeader header_;
-	Type value_;
-};
-
-//
-// Implementation
-//
-
-template <typename Type> inline Type* StructCell<Type>::operator->() {
-	return &value_;
-}
-
-template <typename Type> inline constexpr const Type* StructCell<Type>::operator->() const {
-	return &value_;
-}
-
-template <typename Type> inline Type& StructCell<Type>::operator*() {
-	return value_;
-}
-
-template <typename Type> inline constexpr const Type& StructCell<Type>::operator*() const {
-	return value_;
-}
+template <typename T>
+class StructCell : public NativeCell, public Pith::Box<T> {};
 
 }  // namespace Om
 
