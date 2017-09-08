d4e9a09a4d5b929e64f71b52ac590f1eeed8cb16
Robert Young <rwy0717@gmail.com>
2017-09-08 11:26:37 -0400

    Comment out Om::Value

--- om/include/Om/Value.hpp
+++ om/include/Om/Value.hpp
@@ -1,6 +1,10 @@
 #if !defined(OM_VALUE_HPP_)
 #define OM_VALUE_HPP_
 
+#if 0
+
+#include <Om/Config.hpp>
+#include <Pith/Address.hpp>
 #include <cstdint>
 
 namespace Om {
@@ -10,39 +14,22 @@ class Value {
 public:
 	constexpr inline Value();
 
-	/// Construct
-	template <typename Type> constexpr inline Value(const Type& x);
+	constexpr inline Value(Pith::Address addr);
 
 	/// Cast this to Type.
-	template <typename Type> constexpr Type to() const;
+	template <typename Type>
+	constexpr auto to() const -> Type const;
 
 private:
 	std::uintptr_t value_{0};
 };
 
-static_assert(sizeof Value == sizeof Pith::Address, "Value must be pointer-width");
-
-//
-// Implementation
-//
-
-constexpr inline Value::Value() : value_{0} {
-}
-
-/// TODO: Assert Type is convertible to uintptr_t
-template <typename Type>
-constexpr inline Value::Value(const Type& x)
-	: value_{(std::uintptr_t)x} {
-		  // static_assert(sizeof(Type) == sizeof(std::uintptr_t), "Values must be
-		  // pointer-width.");
-	  };
-
-/// TODO: Assert uintptr_t is convertible to Type
-template <typename Type> constexpr inline Type Value::to() const {
-	// static_assert(sizeof(Type) == sizeof(std::uintptr_t), "Values are pointer-width.");
-	return static_cast<Type>(value_);
-}
+static_assert(sizeof(Value) == sizeof(Pith::Address), "Value must be pointer-width");
 
 }  // namespace Om
+
+#include <Om/Value.inl.hpp>
+
+#endif // 0
 
 #endif  // OM_VALUE_HPP_
---
+++ om/include/Om/Value.inl.hpp
@@ -0,0 +1,29 @@
+#ifndef OM_VALUE_INL_HPP_
+#define OM_VALUE_INL_HPP_
+
+#include <Om/Value.hpp>
+
+namespace Om {
+
+constexpr inline Value::Value() : value_{0} {
+}
+
+/// TODO: Assert Type is convertible to uintptr_t
+template <typename Type>
+constexpr inline Value::Value(const Type& x)
+	: value_{(std::uintptr_t)x}
+{
+		  // static_assert(sizeof(Type) == sizeof(std::uintptr_t), "Values must be
+		  // pointer-width.");
+}
+
+/// TODO: Assert uintptr_t is convertible to Type.
+template <typename Type>
+constexpr inline auto Value::to() const -> Type {
+	// static_assert(sizeof(Type) == sizeof(std::uintptr_t), "Values are pointer-width.");
+	return static_cast<Type>(value_);
+}
+
+} // namespace Om
+
+#endif // OM_VALUE_INL_HPP_
