8c92a973cbac1946ecac317896d95236a4f63424
Robert Young <rwy0717@gmail.com>
2017-09-08 11:40:12 -0400

    Add TestStackRoot

---
+++ om/test/TestStackRoot.cpp
@@ -0,0 +1,12 @@
+#include <Om/Config.hpp>
+#include <Om/StackRoot.hpp>
+#include <gtest/gtest.h>
+
+namespace Om {
+namespace Test {
+
+TEST(TeststackRoot, base) {
+}
+
+}  // namespace Test
+}  // namespace Om
