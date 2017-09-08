81b6ff0b4df28aa60a5c59bcf704f93a36fc7215
Robert Young <rwy0717@gmail.com>
2017-09-08 11:39:00 -0400

    Clear out TestSystem

--- om/test/TestSystem.cpp
+++ om/test/TestSystem.cpp
@@ -1,11 +1,12 @@
 #include <Om/System.hpp>
 #include <gtest/gtest.h>
 
-TEST() {
-	SystemConfig config;
-	config.minimumHeapSize_ = kib(1);
-	config.maximumHeapSize_ = mib(100);
-	System system(config);
-	EXPECT_EQ(0, system.init());
-	EXPECT_EQ(0, system.kill());
+namespace Om {
+namespace Test {
+
+TEST(TestSystem, base) {
+	// TODO: Write system up/down test
 }
+
+} // namespace Test
+} // namespace Om
