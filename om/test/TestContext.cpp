ebf57d833773b2c93882ef58c8d5a9992e68a792
Robert Young <rwy0717@gmail.com>
2017-09-08 11:39:31 -0400

    Init the system properly and fix compile errors

--- om/test/TestContext.cpp
+++ om/test/TestContext.cpp
@@ -1,5 +1,6 @@
 #include <Om/Config.hpp>
 #include <Om/Context.hpp>
+#include <Om/ActiveContext.hpp>
 #include <gtest/gtest.h>
 
 namespace Om {
@@ -7,10 +8,10 @@ namespace Test {
 
 TEST(TestContext, basic) {
 	System system;
-	system.init();
+	system.init(System::defaultConfig());
 	Context cx{system};
 	cx.init();
-	ActiveContext acx{context};
+	ActiveContext acx{cx};
 }
 
 } // namespace Test
