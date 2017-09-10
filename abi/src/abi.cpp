755b8a8f2757a604ff2e634049a65c57a3ceeda0
Robert Young <rwy0717@gmail.com>
2017-09-10 10:13:02 -0400

    Make abi compile again
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- abi/src/abi.cpp
+++ abi/src/abi.cpp
@@ -1,61 +1,15 @@
 #include <Ab/Version.hpp>
+#include <Ab/Process.hpp>
+#include <Pith/Assert.hpp>
 #include <iostream>
 
-#if 0
-template <> bool init(Klass* k, Klass* parentKlass) {
-	VirtualMachine vm;
-
-	heap->allocate<Klass>();
-	std::cout << "initializing klass" << std::endl;
-	k->klass = parentKlass;
-	return true;
-}
-
-bootstrap(VirtualMachine& vm) {
-	auto cx = vm.access();
-	auto mx = vm.memory.access();
-
-	auto klassKlass = allocate<Klass>(mx);
-	klass->klass = klass;
-};
-
-Shape {
-
-};
-
-BaseShape {
-
-};
-
-
-thread_entry() {
-	Ab::Context cx;
-	cx.init();
-
-	cx.kill();
-}
-
-#endif
-
-extern "C" auto main(int argc, char** argv) -> int {
+extern "C" auto main([[gnu::unused]] int argc, [[gnu::unused]] char** argv) -> int {
 	// Process wide init
-	PITH_ASSERT_SUCCESS(Ab::Process::init());
-
-	// Bring up our interpreter system
-	Ab::System system;
-	PITH_ASSERT_SUCCESS(system.init());
-
-	// Create a thread context
-	Ab::ContextState context(system);
-	PITH_ASSERT_SUCCESS(context.init());
-
-	Ab::ActiveContext cx{context};
+	PITH_ASSERT(Ab::Process::init() == Ab::ProcessError::SUCCESS);
 
 	std::cout << Ab::Version::ABIGAIL << std::endl;
 
 	// tear down in reverse order.
-	PITH_ASSERT_SUCCESS(context.kill());
-	PITH_ASSERT_SUCCESS(system.kill());
-	PITH_ASSERT_SUCCESS(Ab::Process::kill());
+	PITH_ASSERT(Ab::Process::kill() == Ab::ProcessError::SUCCESS);
 	return 0;
 };
