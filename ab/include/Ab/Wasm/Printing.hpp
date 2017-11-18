369f7f8bfd4ee73921312eb165fa792e5f2a6142
Robert Young <rwy0717@gmail.com>
2017-11-18 12:22:51 -0500

    Add skeleton WAST printing header
    We're going to move all the Binary IR printing code to a new header.
    
    Signed-off-by: Robert Young <rwy0717@gmail.com>

---
+++ ab/include/Ab/Wasm/Printing.hpp
@@ -0,0 +1,18 @@
+#ifndef AB_WASM_PRINTING_HPP_
+#define AB_WASM_PRINTING_HPP_
+
+#include <Pith/SexprPrinter.hpp>
+#include <Ab/Wasm/Binary/Ir.hpp>
+#include <Ab/Wasm/Binary/Expression.hpp>
+
+namespace Ab {
+
+using namespace Bin = Binary;
+
+// https://github.com/WebAssembly/spec/tree/master/interpreter/#s-expression-syntax
+
+} // namespace Ab
+
+// #include <Ab/Wasm/Printing.inl.hpp>
+
+#endif // AB_WASM_PRINTING_HPP_
