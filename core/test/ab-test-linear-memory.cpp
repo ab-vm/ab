f2020c06a3c0718c8f62eeb06863b32cb6a354fe
Robert Young <rwy0717@gmail.com>
2019-09-15 00:45:39 -0400

    Remove basic test from linear memory test
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- core/test/ab-test-linear-memory.cpp
+++ core/test/ab-test-linear-memory.cpp
@@ -1,15 +1,12 @@
 #include <Ab/Config.hpp>
 #include <Ab/LinearMemory.hpp>
-#include <Ab/Test/BasicTest.hpp>
 #include <gtest/gtest.h>
 
 namespace Ab::Test {
 
-class LinearMemoryTest : public BasicTest {};
+TEST(LinearMemoryTest, init_and_kill) { LinearMemory m; }
 
-TEST_F(LinearMemoryTest, init_and_kill) { LinearMemory m; }
-
-TEST_F(LinearMemoryTest, grow_three_times) {
+TEST(LinearMemoryTest, grow_three_times) {
 	LinearMemory m;
 	for (std::size_t i = 0; i < 3; i++) {
 		m.grow();
