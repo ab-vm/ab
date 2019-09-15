685c75dafd3ccc21c6f08eef3d653199954e11f6
Robert Young <rwy0717@gmail.com>
2019-09-15 00:19:18 -0400

    Rename linear memory test
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- core/test/ab-test-linear-memory.cpp
+++ core/test/ab-test-linear-memory.cpp
@@ -3,21 +3,17 @@
 #include <Ab/Test/BasicTest.hpp>
 #include <gtest/gtest.h>
 
-namespace Ab {
-namespace Test {
+namespace Ab::Test {
 
 class LinearMemoryTest : public BasicTest {};
 
-TEST_F(LinearMemoryTest, initAndKill) {
-	LinearMemory m{};
-}
+TEST_F(LinearMemoryTest, init_and_kill) { LinearMemory m; }
 
-TEST_F(LinearMemoryTest, grow3) {
-	LinearMemory m{};
+TEST_F(LinearMemoryTest, grow_three_times) {
+	LinearMemory m;
 	for (std::size_t i = 0; i < 3; i++) {
 		m.grow();
 	}
 }
 
-}  // namespace Test
-}  // namespace Ab
+}  // namespace Ab::Test
