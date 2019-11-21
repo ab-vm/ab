647cb98469eb5e7bf72e78df8cc0f999334ffb80
Robert Young <rwy0717@gmail.com>
2019-11-21 10:48:25 -0500

    Add pop function to vector utilities
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- util/include/Ab/VectorUtilities.hpp
+++ util/include/Ab/VectorUtilities.hpp
@@ -5,12 +5,22 @@
 
 namespace Ab {
 
-/// Emplace an element onto the back of the vector, returning a reference to the new element.
+/// Push an element onto the back of the vector.
+/// @returns a reference to the new element.
 ///
 template <typename T, typename A, typename... Args>
 T& push(std::vector<T, A>& vector, Args&&... args) {
 	vector.emplace_back(std::forward<Args>(args)...);
-	return vector[vector.size() - 1];
+	return vector.back();
+}
+
+/// Pop a value from the back of a vector, and return it.
+///
+template <typename T, typename A>
+T pop(std::vector<T, A>& vector) {
+	T value(std::move(vector.back()));
+	vector.pop_back();
+	return value;
 }
 
 } // namespace Ab
