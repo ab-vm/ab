739741885ab76fae502b16e7616da56e4756fc50
Robert Young <rwy0717@gmail.com>
2019-11-23 18:20:02 -0500

    Implement varuint encoding
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- util/include/Ab/VarInt.hpp
+++ util/include/Ab/VarInt.hpp
@@ -18,12 +18,21 @@ struct EncodingError : public std::runtime_error {
 ///
 template <std::size_t N, typename T>
 inline void append_varuint(ByteBuffer& buffer, T value) {
-	for (std::size_t shift = 0; shift < N; shift += 7) {
-		buffer.append(static_cast<std::uint8_t>(value >> shift));
-	}
-	if (value & (std::numeric_limits<T>::max() << 7)) {
-		throw EncodingError("Encoding error: number too large");
-	}
+	static_assert(std::is_integral_v<T>);
+	static_assert(std::is_unsigned_v<T>);
+	static_assert(N <= sizeof(T) * 8);
+
+	constexpr std::uint8_t data_mask = 0b0111'1111;
+	constexpr std::uint8_t flag_mask = 0b1000'0000;
+
+	do {
+		std::uint8_t byte = std::uint8_t(value) & data_mask;
+		value = value >> 7;
+		if (value != 0) {
+			byte = byte | flag_mask;
+		}
+		buffer.append(byte);
+	} while (value != 0);
 }
 
 /// Append a 7-bit number encoded in uleb128 to the buffer.
