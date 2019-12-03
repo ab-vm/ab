1c09eb12822bf7c0d4bcebffde0342ddbccc10a0
Robert Young <rwy0717@gmail.com>
2019-12-02 21:20:04 -0500

    Update Module API to use std::span
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- core/include/Ab/Module.hpp
+++ core/include/Ab/Module.hpp
@@ -1,12 +1,16 @@
-#ifndef AB_Module_HPP_
-#define AB_Module_HPP_
+#ifndef AB_MODULE_HPP_
+#define AB_MODULE_HPP_
 
 #include <Ab/Config.hpp>
+
 #include <Ab/Address.hpp>
 #include <Ab/Assert.hpp>
-#include <Ab/Context.hpp>
+#include <Ab/Bytes.hpp>
 #include <Ab/Func.hpp>
+#include <Ab/VectorUtilities.hpp>
+
 #include <cstddef>
+#include <span>
 #include <string>
 #include <vector>
 
@@ -32,24 +36,24 @@ class ModuleStorage {
 public:
 	using Deleter = void (*)(void*);
 
-	explicit ModuleStorage(Byte* address) noexcept : ModuleStorage(address, &std::free) {}
+	explicit ModuleStorage(std::span<Byte> bytes) noexcept : ModuleStorage(bytes, &std::free) {}
 
-	ModuleStorage(Byte* address, Deleter deleter) noexcept
-		: address_(address), deleter_(deleter) {}
+	ModuleStorage(std::span<Byte> bytes, Deleter deleter) noexcept
+		: bytes_(bytes), deleter_(deleter) {}
 
 	ModuleStorage(const ModuleStorage&) = delete;
 
 	ModuleStorage(ModuleStorage&& other) noexcept {
-		address_ = other.address_;
+		bytes_   = other.bytes_;
 		deleter_ = other.deleter_;
 
-		other.address_ = nullptr;
+		other.bytes_   = std::span<Byte>();
 		other.deleter_ = nullptr;
 	}
 
-	~ModuleStorage() {
-		if (address_) {
-			deleter_(address_);
+	~ModuleStorage() noexcept {
+		if (address() && deleter_) {
+			deleter_(address());
 		}
 	}
 
@@ -57,14 +61,18 @@ public:
 
 	ModuleStorage& operator=(ModuleStorage&&) = delete;
 
-	Byte* address() const noexcept { return address_; }
+	Byte* address() const noexcept { return bytes_.data(); }
 
 	Deleter deleter() const noexcept { return deleter_; }
 
+	std::span<Byte> bytes() const noexcept { return bytes_; }
+
 private:
-	Byte* address_;
+	std::span<Byte> bytes_;
 	Deleter deleter_;
 };
+
+using FuncTable = std::vector<Func>;
 
 /// A stateless representation of bytes.
 ///
@@ -78,14 +86,36 @@ public:
 
 	~Module() noexcept = default;
 
-	Byte* bytes() const noexcept { return storage_.address(); }
+	std::span<Byte> bytes() const noexcept { return storage_.bytes(); }
 
 	ModuleStorage& storage() noexcept { return storage_; }
 
 	const ModuleStorage& storage() const noexcept { return storage_; }
 
+	FuncTable& func_table() noexcept { return func_table_; }
+
+	const FuncTable& func_table() const noexcept { return func_table_; }
+
+	std::vector<FuncType>& type_table() noexcept { return type_table_; }
+
+	const std::vector<FuncType>& type_table() const noexcept { return type_table_; }
+
+	std::vector<std::uint32_t>& func_types() noexcept { return func_types_; }
+
+	const std::vector<std::uint32_t>& func_types() const noexcept { return func_types_; }
+
+	/// Get the type for the nth function in the module.
+	///
+	const FuncType& type_for(std::uint32_t funcidx) const noexcept {
+		std::uint32_t typeidx = func_types_.at(funcidx);
+		return type_table_.at(typeidx);
+	}
+
 private:
 	ModuleStorage storage_;
+	FuncTable func_table_;
+	std::vector<FuncType> type_table_;
+	std::vector<std::uint32_t> func_types_;
 };
 
 /// Module Instance.
@@ -93,38 +123,39 @@ private:
 ///
 class ModuleInst {
 public:
-	ModuleInst(const std::shared_ptr<Module>& module) : module_(module) {}
+	ModuleInst(const std::shared_ptr<Module>& module) : module_(module) { initialize(); }
 
-	ModuleInst(std::shared_ptr<Module>&& module) : module_(std::move(module)) {}
+	ModuleInst(std::shared_ptr<Module>&& module) : module_(std::move(module)) { initialize(); }
 
 	/// Obtain the underlying, stateless representation of the module.
 	/// Note that the module may be shared by multiple instantiations.
 	///
-	const std::shared_ptr<Module>& module() const noexcept { return module_; }
+	const std::shared_ptr<Module>& shared_module() const noexcept { return module_; }
 
 	/// Obtain the underlying bytes that represent the module.
 	///
-	const Byte* bytes() const noexcept { return module_->bytes(); }
+	std::span<Byte> bytes() const noexcept { return module_->bytes(); }
 
-	std::vector<FuncInst>& func_table() noexcept { return func_table_; }
+	std::vector<FuncInst>& func_inst_table() noexcept { return func_inst_table_; }
 
-	const std::vector<FuncInst>& func_table() const noexcept { return func_table_; }
+	const std::vector<FuncInst>& func_inst_table() const noexcept { return func_inst_table_; }
 
-	/// Helper to grab a function by index.
+	/// Helper to grab a function instance by index.
 	///
-	FuncInst* func(std::size_t index) noexcept {
-		AB_ASSERT(index <= func_table_.size());
-		return &func_table_[index];
+	FuncInst* func_inst(std::size_t index) noexcept {
+		AB_ASSERT(index < func_inst_table_.size());
+		return &func_inst_table_[index];
 	}
 
-	/// Helper to grab a function by index.
+	/// Helper to grab a function instance by index.
 	///
-	const FuncInst* func(std::size_t index) const noexcept {
-		AB_ASSERT(index <= func_table_.size());
-		return &func_table_[index];
+	const FuncInst* func_inst(std::size_t index) const noexcept {
+		AB_ASSERT(index < func_inst_table_.size());
+		return &func_inst_table_[index];
 	}
 
-#if 0
+#if 0  /////////////////////////////////////////////////////////////////////////
+
 	/// Find an function by name.
 	///
 	Func* find_function(std::string_view name) noexcept {
@@ -154,12 +185,21 @@ public:
 	const Func* function_for(const ExportEntry& entry) const noexcept {
 		return func_table_.at(entry.index);
 	}
-#endif  // 0
+
+#endif  ////////////////////////////////////////////////////////////////////////
 
 	const std::shared_ptr<Module> module_;
-	std::vector<FuncInst> func_table_;
+	std::vector<FuncInst> func_inst_table_;
+
+private:
+	void initialize() {
+		func_inst_table_.reserve(module_->func_table().size());
+		for (auto& func : module_->func_table()) {
+			func_inst_table_.emplace_back(&func);
+		}
+	}
 };
 
 }  // namespace Ab
 
-#endif  // AB_Module_HPP_
+#endif  // AB_MODULE_HPP_
